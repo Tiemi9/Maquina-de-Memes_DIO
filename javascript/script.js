a017d5ca7c6ebb60eb09ab132988f53b58c2b3a1
Tiemi9 <cristina_tiemi.social@ayahoo.com>
2022-11-18 16:10:50 -0300

    Add Memes List

---
+++ javascript/script.js
@@ -0,0 +1,67 @@
+async function mapImageList() {
+    const memesObject = [
+        {
+            "name": "Gato Contocionista",
+            "path": "img/gato-contorcionista.jpg"
+        },
+        {
+            "name": "Gato Risonho",
+            "path": "img/gato-sorrindo.jpg"
+        },
+        {
+            "name": "Gato Assustado",
+            "path": "img/gato-assustado.jpg"
+        },
+        {
+            "name": "Gato Constrangido",
+            "path": "img/gato-constrangido.jpg"
+        },
+        {
+            "name": "Gato Arteiro",
+            "path": "img/gato-fiz-arte.jpg"
+        },
+        {
+            "name": "Gato Intimidador",
+            "path": "img/gato-intimidador.jpg"
+        },
+        {
+            "name": "Gato Linguarudo",
+            "path": "img/gato-linguarudo.jpg"
+        },
+        {
+            "name": "Gato Pensativo",
+            "path": "img/gato-pensativo.jpg"
+        },
+        {
+            "name": "Gato Safado",
+            "path": "img/gato-safado.jpg"
+        },
+        {
+            "name": "Gato Sorrindo",
+            "path": "img/gato-sorrindo.jpg"
+        },
+        {
+            "name": "Gato Tímido",
+            "path": "img/gato-timido.jpg"
+        }
+    ]
+    return memesObject
+}
+
+async function creatGallery(imageList) {
+    const memeSelector = document.querySelector('#memes-list')
+    imageList.forEach(picture => {
+        let newOption = document.createElement('Option')
+        newOption.text = picture.name.toUpperCase()
+        newOption.value = picture.path
+        memeSelector.appendChild(newOption)
+    });
+}
+
+async function main() {
+    const memesImageList = await mapImageList()
+    await creatGallery(memesImageList)
+}
+
+main();
+
